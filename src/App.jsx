1362e0dd2d806d72540afb1bec4f3d662e0c5ede
Christabel Obi-Nwosu <157861550+Christabel091@users.noreply.github.com>
2025-06-10 22:37:15 -0700

    added fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
-import { useState, useEffect  } from 'react'
-import MovieList from './component/MovieList'
-import './App.css'
-import Header from './component/Header'
-import Footer from './component/Footer'
+import { useState, useEffect } from "react";
+import MovieList from "./component/MovieList";
+import "./App.css";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 const App = () => {
   //initial movie load
-   const [pageNumber, setPageNumber] = useState(1);
-    const API_KEY = import.meta.env.VITE_API_KEY;
-    const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${pageNumber}`;
-    const [movies, setMovies] = useState([]);
-     const [searchInput, setSearchInput] = useState('');
-    const [originalMovies, setOriginalMovies] = useState(movies);
-    //function to load initial movies
-    useEffect(() =>{
-        const fetchMovies = async ()=>{
-            const res = await fetch(URL);
-            const data = await res.json();
-            
-            setMovies(prevMovies => [...prevMovies, ...data.results]);
-            setOriginalMovies(prevMovies => [...prevMovies, ...data.results]);
-        }
-        fetchMovies();
-    }, [pageNumber]);
-    const handlePageLoad = () => setPageNumber(pageNumber+1);
-    
-    //display when search is made
-    let searchForMovie = () =>{
-      const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&query=${encodeURIComponent(searchInput)}&page=${pageNumber}`;
-      if (searchInput === ''){
-          setMovies(originalMovies);
-      }else{
-        console.log("search function ran");
-        const fetcSearchMovies = async ()=>{
+  const [pageNumber, setPageNumber] = useState(1);
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${pageNumber}`;
+  const [movies, setMovies] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [originalMovies, setOriginalMovies] = useState(movies);
+  //function to load initial movies
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch(URL);
+      const data = await res.json();
+
+      setMovies(data.results);
+      setOriginalMovies(data.results);
+    };
+    fetchMovies();
+  }, [URL]);
+  const handlePageLoad = () => setPageNumber(pageNumber + 1);
+
+  //display when search is made
+  let searchForMovie = () => {
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      searchInput
+    )}&page=${pageNumber}`;
+
+    if (searchInput === "") {
+      setMovies(originalMovies);
+    } else {
+      console.log("search function ran");
+      const fetcSearchMovies = async () => {
         const res = await fetch(URL);
         const data = await res.json();
-        setMovies(data.results);   
-        
-        }
-        fetcSearchMovies();
-      }
+        setMovies(data.results);
+      };
+      fetcSearchMovies();
     }
+  };
   return (
     <div className="App">
-      <Header searchForMovie= {searchForMovie} searchInput={searchInput} setSearchInput={setSearchInput} />
-      <MovieList handlePageLoad={handlePageLoad} movies ={movies}/>
+      <Header
+        searchForMovie={searchForMovie}
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
+      <MovieList handlePageLoad={handlePageLoad} movies={movies} />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from "react";
 import Button from "./Button";
 
-let Search = (props) =>{
-    let handleSearchChange = (event) =>{
-        props.setSearchInput(event.target.value);
-    }
-   let clearSearchIput = () =>{
-        props.setSearchInput('');
-   }
-    return(
-        <div className="search">
-            <input type="text" value={props.searchInput} onChange={handleSearchChange} placeholder="Search" />
-            <Button onclick={props.searchForMovie} title = "search" />
-            <Button onclick={clearSearchIput} title="Now playing" />
-        </div>
-    )
-}
+let Search = (props) => {
+  let handleSearchChange = (event) => {
+    props.setSearchInput(event.target.value);
+  };
+  let clearSearchIput = () => {
+    props.setSearchInput("");
+  };
+  return (
+    <div className="search">
+      <input
+        type="text"
+        value={props.searchInput}
+        onChange={handleSearchChange}
+        placeholder="Search"
+      />
+      <Button onclick={props.searchForMovie} title="search" />
+      <Button onclick={clearSearchIput} title="Now playing" />
+    </div>
+  );
+};
 export default Search;
